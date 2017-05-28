bba0a66576bf01af53aa86289f8cf9d0df45b40f
Dave Beck <beck.dac@live.com>
2017-05-28 01:33:12 -0700

    code refactor for more explicit

--- audio_stereo_out_tb.v
+++ audio_stereo_out_tb.v
@@ -4,9 +4,14 @@ module audio_stereo_out_tb();
 	localparam PCM_CLOCK_PERIOD_NS = 20;
 	localparam AUDIO_CLOCK_PERIOD_NS = 8.89245;
 
-	reg aclr = 1, clk_pcm = 0, clk_audio = 0;
-	reg [7:0] left = 0, right = 0;
-	wire left_out, right_out, fifo_full;
+	reg aclr = 1;
+	reg clk_pcm = 0;
+   	reg	clk_audio = 0;
+	reg [7:0] left = 0;
+	reg [7:0] right = 0;
+	wire left_out;
+	wire right_out;
+	wire fifo_full;
 	reg stereo_pcm_rdy = 0;
 
 	always #(PCM_CLOCK_PERIOD_NS/2) clk_pcm <= !clk_pcm;
@@ -32,6 +37,7 @@ module audio_stereo_out_tb();
 			aclr <= 1;
 			#PCM_CLOCK_PERIOD_NS
 			left = 8'd127;
+			right = 8'd0;
 			stereo_pcm_rdy <= 1;
 			#PCM_CLOCK_PERIOD_NS
 			stereo_pcm_rdy <= 0;
